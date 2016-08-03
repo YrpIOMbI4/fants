712bab2b69d68f3ea7cf5ee9170a91a227aaece9
Drobot <YrpIOMbI4@mail.ru>
2016-08-04 00:01:46 +0300

    paragraphs

--- src/components/Fant.js
+++ src/components/Fant.js
@@ -1,14 +1,20 @@
 import React, { Component, PropTypes } from 'react'
 
 class Fant extends Component {
+
+	parseParagraph(string) {
+		return string.split('#')
+	}
+
 	render() {
 
 		const { fant } = this.props
+		const paragraphs = this.parseParagraph(fant.description)
 
 		return (
 			<div className="Fant">
 				<h2 className="Fant__title">{fant.title}</h2>
-				<p className="Fant__description">{fant.description}</p>
+				{paragraphs.map(par => <p className="Fant__description">{par}</p>)}
 			</div>
 		)
 	}
